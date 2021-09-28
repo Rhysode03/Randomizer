8ec9b08ac6b169f5367d0ec95dca66a1aa239211
Kyle Calder <kylebcalder03@gmail.com>
2021-09-27 22:30:30 -0600

    Added ability to add and delete games

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,18 @@
   <p>{{chosenName}}</p>
   </div>
   <div class="row d-flex justify-content-center">
-  <button class="btn-lg btn-primary" @click="picker">Choose!</button>
+  <button class="btn-lg btn-primary mb-5" @click="picker">Choose!</button>
+  </div>
+  <div class="container">
+  <div class="row mt-5 d-flex justify-content-center">
+  <input id="game" placeholder="Add a game to the list"></div>
+  <div class="row d-flex justify-content-center mt-1">
+  <button class="btn btn-primary ml-2" @click="addToList">Add a game</button>
+  <button class="btn btn-danger ml-2" @click="deleteList">Delete games</button>
+  </div>
+  <div class="row d-flex justify-content-center">
+  <div>{{list}}</div>
+  </div>
   </div>
 </div>
 
@@ -34,13 +45,24 @@ export default{
           'Quake',
           'Quake II',
           'Quake III'],
-    chosenName: 'Randomizer'
+    chosenName: 'Randomizer',
   }
   },
   methods: {
     picker(){
       var chosenNumber = Math.floor(Math.random() * this.list.length);
       this.chosenName = this.list[chosenNumber];
+      if (this.list.length <= 0) {
+        this.chosenName = 'Please add a game to the list'
+      }
+    },
+    addToList(){
+      this.list.push(document.getElementById("game").value);
+    },
+    deleteList(){
+        for (let i = this.list.length -1; i >= 0; i--){
+          this.list.splice(i, 1);
+        }
     }
 }
 }
